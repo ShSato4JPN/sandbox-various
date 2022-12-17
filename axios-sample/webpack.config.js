d000b137e779504a41ae4422964f2a820c00bc4c
ShSato4JPN <shsato4.private.1221@gmail.com>
2022-12-17 13:34:52 +0900

    feat: add test sources

---
+++ axios-sample/webpack.config.js
@@ -0,0 +1,16 @@
+/** ↓ エディタで補完を効かせるための JSDoc */
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        // 拡張子 js のファイル（正規表現）
+        test: /\.js$/,
+        // ローダーの指定
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
