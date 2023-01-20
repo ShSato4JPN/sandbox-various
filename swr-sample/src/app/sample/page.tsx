f044ac8244ca3b1c79319fe1ddac862083f2119c
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-01-21 00:54:01 +0900

    feat: no swr

--- swr-sample/src/app/sample/page.tsx
+++ swr-sample/src/app/sample/page.tsx
@@ -1,15 +1,30 @@
-import useSWR from "swr";
+"use client";
+import { useState, useEffect } from "react";
 
-const fetcher = (url: any) => fetch(url).then((res) => res.json());
+//const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
-export default async function Home() {
-  const data = await fetch("http://localhost:3001/api/post").then((res) =>
-    res.json()
+const cache = new Map<string, string>();
+
+export default function Home() {
+  const [value, setValue] = useState<string>(
+    cache.has("key") ? cache.get("key") || "key" : "no data"
   );
+
+  useEffect(() => {
+    fetch("/api/post")
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          cache.set("kye", JSON.stringify(data));
+          setValue(JSON.stringify(data));
+        }, 5000);
+      })
+      .catch((e) => setValue(e.message));
+  }, []);
 
   return (
     <>
-      <p>{JSON.stringify(data)}</p>
+      <p>{value}</p>
     </>
   );
 }
