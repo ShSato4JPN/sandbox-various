95a994d31814741fc25dd310d1ad7e79ba6a2881
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-03-15 23:38:17 +0900

    feat: add date-fns

---
+++ date-fns-sample/src/index.js
@@ -0,0 +1,12 @@
+console.log();
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
+
+console.log(format(new Date(), "'Today is a' eeee"));
+
+console.log(
+  formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
+);
+
+console.log(formatRelative(subDays(new Date(), 3), new Date()));
+
+console.log("hello");
