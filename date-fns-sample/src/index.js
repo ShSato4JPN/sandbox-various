7a150ad9caa472e7d3aa80e4806e1a33177873ea
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-03-15 23:50:13 +0900

    feat: add test code on date-fns

--- date-fns-sample/src/index.js
+++ date-fns-sample/src/index.js
@@ -1,12 +1,24 @@
 console.log();
-import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import {
+  format,
+  formatDistance,
+  formatRelative,
+  subDays,
+  parseISO,
+} from "date-fns";
 
-console.log(format(new Date(), "'Today is a' eeee"));
+//console.log(format(new Date(), "'Today is a' eeee"));
+// console.log(
+//   formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
+// );
+//console.log(formatRelative(subDays(new Date(), 3), new Date()));
 
-console.log(
-  formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
+const foo = parseISO("2023-01-10T00:00+09:00", "yyyy-MM-dd");
+//console.log(foo);
+//console.log(format(foo, "yyyy-MM-dd"));
+
+const bar = format(
+  parseISO("2023-01-10T00:00+09:00", "yyyy-MM-ddTHH:mm:ss", new Date()),
+  "yyyy-MM-dd"
 );
-
-console.log(formatRelative(subDays(new Date(), 3), new Date()));
-
-console.log("hello");
+console.log(bar);
