b5ebd6aecfbf38ad9cecaaae8a6af642c1b34056
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-08-27 23:52:36 +0900

    セルの結合的なことをやってみた

--- subgrid-sample/src/App.tsx
+++ subgrid-sample/src/App.tsx
@@ -13,25 +13,33 @@ function App() {
     <div
       className="grid"
       style={{
-        gridTemplateColumns: `repeat(${data.length}, 1fr)`,
+        gridTemplateColumns: `repeat(${data.length}, auto)`,
         gridTemplateRows: `repeat(${data[0].length}, auto)`,
       }}
     >
-      {data.map((col, colIndex) => (
-        <div
-          key={colIndex}
-          className="grid grid-rows-subgrid"
-          style={{
-            gridRow: `1 / ${data[0].length + 1}`,
-          }}
-        >
-          {col.map((cell, rowIndex) => (
-            <div key={rowIndex} className="whitespace-pre-wrap">
-              {cell}
-            </div>
-          ))}
-        </div>
-      ))}
+      {data.map((col, colIndex) => {
+        return (
+          <div
+            key={colIndex}
+            className="grid grid-rows-subgrid"
+            style={{
+              gridRow: `1 / ${data[0].length + 1}`,
+            }}
+          >
+            {colIndex % 2 === 0 ? (
+              <div>test</div>
+            ) : (
+              <>
+                {col.map((cell, rowIndex) => (
+                  <div key={rowIndex} className="border-1 whitespace-pre-wrap">
+                    {cell}
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
