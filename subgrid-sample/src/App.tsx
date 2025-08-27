dfa0c1b3f0dd353b62a0ed635306027b35be10d1
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-08-27 23:09:20 +0900

    initialize

---
+++ subgrid-sample/src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid grid-cols-3">
+      <div>
+        <div>1</div>
+        <div className="h-[300px] bg-orange-200">2</div>
+        <div>3</div>
+      </div>
+      <div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+      <div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
